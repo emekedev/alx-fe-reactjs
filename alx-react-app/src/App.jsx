2733d96d701c7fc33c732586cd374f512f8db378
Nwannadi Emeke <emekedev@gmail.com>
2025-07-13 15:08:54 +0100

    Added app.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,6 +2,7 @@ import WelcomeMessage from './components/WelcomeMessage'
 import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
+import UserProfile from './components/UserProfile'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -14,6 +15,7 @@ function App() {
   
     <>
       <div>
+         <UserProfile name="Alice" age="25" bio="Loves hiking and photography" /> 
         <Header />
         <MainContent />
         <Footer />
