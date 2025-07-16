197d7bdece9285d028665a13563cd714a9d83af0
Nwannadi Emeke <emekedev@gmail.com>
2025-07-16 22:52:57 +0100

    added footer.jsx

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,8 @@
+function Footer(){
+    return(
+        <footer>
+            <p style={{color: 'navy', fontSize: '15px'}}>© 2023 City Lovers</p>
+        </footer>
+    )
+}
+export default Footer;
