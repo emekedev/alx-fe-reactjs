479fa01b998ada7838f074e73bcdd7cb51e2712f
Nwannadi Emeke <emekedev@gmail.com>
2025-07-19 20:06:16 +0100

    Your message

---
+++ alx-react-app-props/src/UserDetails.jsx
@@ -0,0 +1,11 @@
+function UserDetails() {
+    const userData = useContext(UserContext);
+  return (
+    <div>
+      <p>Name: {userData.name}</p>
+      <p>Email: {userData.email}</p>
+    </div>
+  );
+}
+
+export default UserDetails;
