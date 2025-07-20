701bbef9a5e1854ab3a50016416204bdda1ba0f6
Nwannadi Emeke <emekedev@gmail.com>
2025-07-20 13:35:40 +0100

    updated all pages

---
+++ my-company/src/Footer.jsx
@@ -0,0 +1,7 @@
+function Footer() {
+  return (
+    <footer style={{ textAlign: 'center', marginTop: '50px', padding: '20px', backgroundColor: '#f0f0f0' }}>
+      <p>&copy; {new Date().getFullYear()} My Company. All rights reserved.</p>
+    </footer>
+  );
+}
