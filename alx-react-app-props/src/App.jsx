8c7bcb2a2ed11bdb78842456235b57a1df4da0d2
Nwannadi Emeke <emekedev@gmail.com>
2025-07-20 11:31:39 +0100

    updated all files

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -3,7 +3,9 @@ import ProfilePage from './ProfilePage';
 function App() {
    const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
-    return <ProfilePage userData={userData} />;
+   <UserContext.Provider value={userData}>
+      <ProfilePage />
+    </UserContext.Provider>
 }
 
 export default App
