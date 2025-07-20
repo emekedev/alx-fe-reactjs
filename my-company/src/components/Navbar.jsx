ee7bbc9d85d77289a29e856bc4e6760289677ffe
Nwannadi Emeke <emekedev@gmail.com>
2025-07-20 14:38:04 +0100

    updated nav

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav style={{ padding: '10px', background: '#333', color: '#fff' }}>
+    <nav style={{ padding: '10px', backgroundColor: '#333', display: 'flex', justifyContent: 'space-around', color: '#fff' }}>
       <Link to="/" style={{ margin: '0 10px', color: 'white' }}>Home</Link>
       <Link to="/about" style={{ margin: '0 10px', color: 'white' }}>About</Link>
       <Link to="/services" style={{ margin: '0 10px', color: 'white' }}>Services</Link>
