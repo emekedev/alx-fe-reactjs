e477bcfe3c09aa620931215b84defeca7c412d1b
Nwannadi Emeke <emekedev@gmail.com>
2025-07-25 03:36:19 +0100

    updated app.jsx

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,16 @@
+import AddRecipeForm from './components/AddRecipeForm'
+import RecipeList from './components/RecipeList'
+
+function App() {
+
+  return (
+    <div>
+      <AddRecipeForm />
+      <RecipeList />
+    </div>
+    
+    
+  )
+}
+
+export default App
