fd5d8742b3a66f573e115f676668058986626a81
Nwannadi Emeke <emekedev@gmail.com>
2025-07-25 20:18:03 +0100

    updated app.jsx

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -6,6 +6,12 @@ function App() {
 
   return (
     <div>
+      <Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/recipe/:id" element={<RecipeDetails />} />
+    </Routes>
+  </Router>
       <AddRecipeForm />
       <RecipeList />
     </div>
