53fe349e90256667ba642f9e36fff57236f391a3
Nwannadi Emeke <emekedev@gmail.com>
2025-08-07 23:38:18 +0100

    another update

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -2,7 +2,7 @@
 function UserProfile() {
   return (
     <div className="user-profile bg-gray-100 p-8 max-w-sm, max-w-xs mx-auto, sm:text-lg md:text-xl my-20 rounded-lg shadow-lg md:p-8 sm:p-4 md:max-w-sm">
-      <img src="https://via.placeholder.com/150" alt="User" className='rounded-full w-36, h-36, mx-auto sm:h-24 w-24, md:h-36 w-36 '/>
+      <img src="https://via.placeholder.com/150" alt="User" className='rounded-full w-36, h-36, mx-auto sm:w-24, md:w-36 '/>
       <h1 className='text-xl text-blue-800 my-4'>John Doe</h1>
       <p className='text-gray-600 text-base sm:text-sm md:text-base'>Developer at Example Co. Loves to write code and explore new technologies.</p>
     </div>
