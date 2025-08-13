df1d484371c56be3c0cd9ba11aada3425654bfed
Nwannadi Emeke <emekedev@gmail.com>
2025-08-13 23:12:59 +0100

    added new changes

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -10,6 +10,21 @@ export default function Homepage() {
 
   return (
     <div className="p-6">
+
+         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+        <div className="bg-white rounded shadow p-4">
+          <h2 className="text-lg font-semibold">Recipe 1</h2>
+          <p>Some description</p>
+        </div>
+        <div className="bg-white rounded shadow p-4">
+          <h2 className="text-lg font-semibold">Recipe 2</h2>
+          <p>Some description</p>
+        </div>
+        <div className="bg-white rounded shadow p-4">
+          <h2 className="text-lg font-semibold">Recipe 3</h2>
+          <p>Some description</p>
+        </div>
+      </div>
       <h1  className="text-2xl font-bold mb-4">Recipe List</h1>
       <ul>
         {recipes.map((recipe) => (
@@ -25,5 +40,7 @@ export default function Homepage() {
         View Recipes
       </button>
     </div>
+
+    
   );
 }
