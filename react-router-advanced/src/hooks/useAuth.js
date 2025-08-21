b24dfce36097a361cfa8f82dfa1ac79d25b8a75f
Nwannadi Emeke <emekedev@gmail.com>
2025-08-21 02:27:23 +0100

    updated files

---
+++ react-router-advanced/src/hooks/useAuth.js
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+// simple custom hook for authentication simulation
+export const useAuth = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const login = () => setIsAuthenticated(true);
+  const logout = () => setIsAuthenticated(false);
+
+  return { isAuthenticated, login, logout };
+};
